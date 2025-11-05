58803b57af7b67f3b879a16141ddb7567c46a038
basketballer4236 <basketballer4236@outlook.com>
2025-11-05 23:12:17 +0000

    Manual code change

--- src/components/auth/AuthModal.jsx
+++ src/components/auth/AuthModal.jsx
@@ -1,5 +1,4 @@
 
-import { base44 } from "@/api/base44Client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
